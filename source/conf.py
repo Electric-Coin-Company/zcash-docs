70663f5ec9c586141909fb06df161ff8b0901a66
mdr0id <marshall@z.cash>
2018-07-17 17:43:58 +0000

    Update conf.py per Garith PR on prior github repo

--- source/conf.py
+++ source/conf.py
@@ -29,7 +29,7 @@ author = u'Paige Peterson & Marshall Gaucher'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'1.0.15'
+release = u'1.1.2'
 
 # -- General configuration ---------------------------------------------------
 
