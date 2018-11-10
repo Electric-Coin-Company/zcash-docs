af327804273793e5ac72d5c60ac3b842bbefa495
Gareth Davies <hello@garethtdavies.com>
2018-11-10 09:24:20 -0800

    Remove duplicate documentation output

--- source/conf.py
+++ source/conf.py
@@ -145,7 +145,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'ZCashDocumentation.tex', u'Zcash Documentation Documentation',
+    (master_doc, 'ZCashDocumentation.tex', u'Zcash Documentation',
      u'Paige Peterson \\& Marshall Gaucher', 'manual'),
 ]
 
@@ -155,7 +155,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'zcashdocumentation', u'Zcash Documentation Documentation',
+    (master_doc, 'zcashdocumentation', u'Zcash Documentation',
      [author], 1)
 ]
 
@@ -166,7 +166,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'ZCashDocumentation', u'Zcash Documentation Documentation',
+    (master_doc, 'ZCashDocumentation', u'Zcash Documentation',
      author, 'ZCashDocumentation', 'One line description of project.',
      'Miscellaneous'),
 ]
