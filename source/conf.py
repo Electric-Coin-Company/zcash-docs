ee167491e48ee9ddc8695f5854a81237dd4bebb0
mdr0id <marshall@z.cash>
2020-08-28 14:55:22 +0000

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -16,6 +16,7 @@
 import os
 import sys
 import sphinx_fontawesome
+import sphinx_sitemap
 #sys.path.insert(0, os.path.abspath('extensions'))
 
 # -- Project information -----------------------------------------------------
