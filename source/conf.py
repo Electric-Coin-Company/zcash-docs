70426f0a54b841d9cfe16616de60ddaad7baf888
mdr0id <marshall@z.cash>
2022-12-02 15:59:52 -0800

    update v5.3.1

--- source/conf.py
+++ source/conf.py
@@ -28,7 +28,7 @@ author = u'ECC'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'5.3.0'
+release = u'5.3.1'
 
 # -- General configuration ---------------------------------------------------
 
